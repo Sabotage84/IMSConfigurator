d5fccf1f69b07de206515819a84ec00dbd07d443
sabotage84@mail.ru <sabotage84@mail.ru>
2018-11-21 16:51:45 +0300

    ADD listBox for outputs

---
+++ IMSConfigurator/IMSConfigurator/ViewModel/OutputModulsVM.cs
@@ -0,0 +1,23 @@
+﻿using IMSConfigurator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IMSConfigurator.ViewModel
+{
+    class OutputModulsVM
+    {
+        public List<Modul> modListMV;
+        public List<Modul> ModListMV { get => modListMV; set => modListMV = value; }
+        public OutputModulsVM()
+        {
+            Moduls m = new Moduls();
+            ModListMV = m.SearchModulsByType(ModulType.Output);
+            modListMV.AddRange(m.SearchModulsByType(ModulType.Input));
+            MessageBox.Show(modListMV[0].Name);
+        }
+    }
+}
