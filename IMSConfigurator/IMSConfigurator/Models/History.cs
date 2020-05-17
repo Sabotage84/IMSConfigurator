52ca05e9944bd276dcb2fc7db4c041963415245c
Sabotage84 <sabotage84@mail.ru>
2020-05-17 22:44:33 +0300

    ADD history class

---
+++ IMSConfigurator/IMSConfigurator/Models/History.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSConfigurator.Models
+{
+    class History : IEnumerable<IMSMetronome>
+    {
+        List<IMSMetronome> imsList = new List<IMSMetronome>();
+        
+        public void AddElement(IMSMetronome item)
+        {
+            imsList.Add(item);
+        }
+        
+        public IEnumerator<IMSMetronome> GetEnumerator()
+        {
+            foreach (var item in imsList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
