0c3bc6957031e010a8890fc03d3f0b1a790646c6
Sabotage84 <sabotage84@mail.ru>
2020-05-18 21:55:00 +0300

    ADD serialeze to history

--- IMSConfigurator/IMSConfigurator/Models/History.cs
+++ IMSConfigurator/IMSConfigurator/Models/History.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
 
 namespace IMSConfigurator.Models
 {
-    class History : IEnumerable<IMSMetronome>
+    [Serializable]
+     class History : IEnumerable<IMSMetronome>
     {
+        public History()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<IMSMetronome>));
+            using (FileStream fs = new FileStream("history.xml", FileMode.OpenOrCreate))
+            {
+
+                try
+                {
+                    imsList = (List<IMSMetronome>)formatter.Deserialize(fs);
+                }
+                catch
+                {
+
+                    MessageBox.Show("History file not foud!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                  
+
+                }
+
+            }
+        }
+        
+        public void SaveHistory()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<IMSMetronome>));
+            using (FileStream fs = new FileStream("histiry.xml", FileMode.OpenOrCreate))
+            {
+                ser.Serialize(fs, imsList);
+            }
+            
+        }
+
+
         List<IMSMetronome> imsList = new List<IMSMetronome>();
         
         public void AddElement(IMSMetronome item)
